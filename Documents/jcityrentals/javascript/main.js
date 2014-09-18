7f7c7ca40e1ebb194dea263d52400c04f62818f4
evabrandenburg <evabrandenburg89@gmail.com>
2014-09-18 17:59:52 -0400

    slide working

--- Documents/jcityrentals/javascript/main.js
+++ Documents/jcityrentals/javascript/main.js
@@ -1,1 +1,11 @@
 /* CURRENTLY IN: javascript/main.js */
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+  	minSlides: 1,
+  	maxSlides: 1,
+  	slideWidth: 500,
+  	slideMargin: 10,
+  	 captions: true
+	});
+
+});
