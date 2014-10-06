dc67904a2e77a456b95fe3e8c5eabbb10753e31e
evabrandenburg <evabrandenburg89@gmail.com>
2014-10-06 15:47:27 -0400

    more

--- Documents/jcityrentals/slider/main.js
+++ Documents/jcityrentals/slider/main.js
@@ -1,3 +1,5 @@
+
+
   $(".next").click( animN );
   $(".prev").click( animP );
 
@@ -7,7 +9,14 @@
   }
 
    function animP(){
-    $(".wrap").append($(".wrap .img").get().reverse());
+    //no $(".wrap .img").reverse(.first().appendTo('.wrap'));
+    //no with or without .get() $(".wrap").first().append((".wrap .img").get().reverse());
+    //nope $(".wrap .img").append((".wrap").reverse());
+    //does not work $(".wrap .img").first().append((".wrap").reverse());
+    // does not work $(".wrap .img").first().append((".wrap").get().reverse());
+    //does not work $(".wrap").first().append().reverse($(".wrap .img"));
+    // works but switches btwn 1 and 3 $(".wrap").append($(".wrap .img").get().reverse());
+    //works but going forward $(".wrap").append($(".wrap .img").first());
     //$(".wrap .img").first().appendTo('.wrap').fadeOut(2000).reverse();
     $(".wrap .img").first();
   }
