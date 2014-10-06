8cc51c2973172b5508fc09fd847d7fd73eb1c73f
evabrandenburg <evabrandenburg89@gmail.com>
2014-10-06 15:53:08 -0400

    added slideshow no content

--- Documents/jcityrentals/javascript/main.js
+++ Documents/jcityrentals/javascript/main.js
@@ -48,5 +48,26 @@ function hide(){
 
 }
 
+$(".next").click( animN );
+  $(".prev").click( animP );
+
+  function animN(){
+    $(".wrap .img").first().appendTo('.wrap');
+    $(".wrap .img").first();
+  }
+
+   function animP(){
+    //no $(".wrap .img").reverse(.first().appendTo('.wrap'));
+    //no with or without .get() $(".wrap").first().append((".wrap .img").get().reverse());
+    //nope $(".wrap .img").append((".wrap").reverse());
+    //does not work $(".wrap .img").first().append((".wrap").reverse());
+    // does not work $(".wrap .img").first().append((".wrap").get().reverse());
+    //does not work $(".wrap").first().append().reverse($(".wrap .img"));
+    // works but switches btwn 1 and 3 $(".wrap").append($(".wrap .img").get().reverse());
+    //works but going forward $(".wrap").append($(".wrap .img").first());
+    //$(".wrap .img").first().appendTo('.wrap').fadeOut(2000).reverse();
+    $(".wrap .img").first();
+  }
+
 
 });
